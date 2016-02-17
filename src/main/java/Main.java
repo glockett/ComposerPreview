89ac759f7e12716a16ea0361ff650c92e64c937d
Gwyn Lockett <gwyn.lockett@theguardian.com>
2016-02-17 14:01:05 +0000

    Changed styles for buttons

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,16 +16,14 @@ import javafx.stage.Stage;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main extends Application {
 
-    private static String USERNAME;
-    private static String PWD;
+    private static String USERNAME= "gwyn.lockett@guardian.co.uk";
+    private static String PWD = "hxxaqkdcmkqthgnn";
     private static String PREVIEW_URL;
     private static final String ENVIRONMENT_URL = "http://viewer.gutools.co.uk/preview/";
     private static final String MAPI_URL = "https://entry.mobile-apps.guardianapis.com/deeplink/items/";
@@ -48,18 +46,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        Properties config = new Properties();
-
-        try{
-            config.load (new FileInputStream ("config.properties"));
-
-            USERNAME = config.getProperty("username");
-            PWD = config.getProperty("password");
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
 
         BorderPane root = new BorderPane();
 
